745bffd9f4821a4a94d4a22981e3b1966e67695e
L. Larrabee Strow <strow@umbc.edu>
2015-02-06 11:39:52 -0500

    Only create directories if needed.

--- airs/scripts/create_airxbcal_rtp.m
+++ airs/scripts/create_airxbcal_rtp.m
@@ -1,4 +1,4 @@
-%function create_airxbcal_rtp(airs_doy, airs_year)
+function create_airxbcal_rtp(airs_doy, airs_year)
 %
 % NAME
 %   create_airxbcal_rtp -- wrapper to process AIRXBCAL to RTP
@@ -14,9 +14,9 @@
 %
 % DISCUSSION (TBD)
 
-addpath /asl/matlib/h4tools
+% addpath /asl/matlib/h4tools
 
-airs_doy = 239; airs_year = 2013;
+%airs_doy = 239; airs_year = 2013;
 
 klayers_exec = '/asl/packages/klayersV205/BinV201/klayers_airs_wetwater';
 sarta_exec   = '/asl/packages/sartaV108/BinV201/sarta_apr08_m140_wcon_nte';
@@ -40,6 +40,8 @@ fnfull = fullfile(indir,fn.name);
 
 % Read the AIRXBCAL file
 [prof, pattr, aux] = read_airxbcal(fnfull);
+
+disp('done readling file')
 
 % Header 
 head = struct;
@@ -70,12 +72,17 @@ if isfield(prof,'zobs')
    prof.zobs(iz) = prof.zobs(iz) * 1000;
 end
 
+% subset by 20 during debugging
+%prof = rtp_sub_prof(prof,1:20:length(prof.rlat));
+
 % Add in Scott's calflag
 matchedcalflag = mkmatchedcalflag(airs_year, airs_doy, prof);
 [prof.calflag cstr] = data_to_calnum_l1bcm(...
           aux.nominal_freq, aux.NeN, ...
           aux.CalChanSummary, ...
           matchedcalflag', prof.rtime, prof.findex);
+
+disp('done with calflag')
 
 % Add in model data
 [prof,head]  = fill_era(prof,head);
@@ -91,20 +98,31 @@ head.pfields = 5;
 % standard routine for sea surface emissivity
 [prof,pattr] = rtp_add_emis(prof,pattr);
 
+disp('done with add emis')
+
 % Save the rtp file
 fn_rtp1 = tempname;
+fn_rtp1 = [fn_rtp1 '.rtp'];
 rtpwrite(fn_rtp1,head,hattr,prof,pattr)
+
+disp('saved first rtp file')
 
 % run klayers
 fn_rtp2 = tempname;
+fn_rtp2 = [fn_rtp2 '.rtp'];
 klayers_run = [klayers_exec ' fin=' fn_rtp1 ' fout=' fn_rtp2 ' > /asl/s1/strow/kout.txt'];
 unix(klayers_run);
 
+disp('done with klayers')
+
 % Run sarta
 fn_rtp3 = tempname;
+fn_rtp3 = [fn_rtp3 '.rtp'];
 sarta_run = [sarta_exec ' fin=' fn_rtp2 ' fout=' fn_rtp3 ];
 %sarta_run = [sarta_exec ' fin=test2_ecmwf.rtp fout=finalfile_ecmwf.rtp'];
 unix(sarta_run);
+
+disp('done with sarta')
 
 % Read in new rcalcs and insert into origin prof field
 [h,ha,p,pa] = rtpread(fn_rtp3);
@@ -123,26 +141,27 @@ prof_dcc   = rtp_sub_prof(prof,idcc);
 prof_rand  = rtp_sub_prof(prof,irand);
 
 % Make directory if needed
-airxbcal_out_dir
-mkdir(airxbcal_out_dir,airs_yearstr);
+if exist(fullfile(airxbcal_out_dir,airs_yearstr)) == 0
+   mkdir(airxbcal_out_dir,airs_yearstr);
+end
 
 rtp_out_fn_head = ['era_airxbcal_day' airs_doystr];
 % Now save the four types of airxbcal files
 
 rtp_out_fn = [rtp_out_fn_head, '_clear.rtp'];
-rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn)
+rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn);
 rtpwrite(rtp_outname,head,hattr,prof_clear,pattr);
 
 rtp_out_fn = [rtp_out_fn_head, '_site.rtp'];
-rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn)
+rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn);
 rtpwrite(rtp_outname,head,hattr,prof_site,pattr);
 
 rtp_out_fn = [rtp_out_fn_head, '_dcc.rtp'];
-rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn)
+rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn);
 rtpwrite(rtp_outname,head,hattr,prof_dcc,pattr);
 
 rtp_out_fn = [rtp_out_fn_head, '_rand.rtp'];
-rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn)
+rtp_outname = fullfile(airxbcal_out_dir,airs_yearstr,rtp_out_fn);
 rtpwrite(rtp_outname,head,hattr,prof_rand,pattr);
 
 
