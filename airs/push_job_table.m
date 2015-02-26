bcc935632e01dcde089f444540802ed243a48b6d
Steven Buczkowski <steven.buczkowski@gmail.com>
2015-02-26 13:59:45 -0500

    Fixed matlab path generation for subroutines

--- airs/push_job_table.m
+++ airs/push_job_table.m
@@ -2,8 +2,8 @@ function push_job_table(iTaskID, sJobName, iDoy, iYear)
 
 sMYSQL = 'mysql -h maya-mgt -u strow -pokdictuv strow';
 sSQL = sprintf(['echo "insert into JobManagement (task_id, year, ' ...
-                'doy, task_name) values (%d, %d, %d, \\"%s\\");"'], iTaskID, iDoy, ...
-               iYear, sJobName);
+                'doy, task_name) values (%d, %d, %d, \\"%s\\");"'], iTaskID, iYear, ...
+               iDoy, sJobName);
 
 [status, cmdout] = system([sSQL ' | ' sMYSQL]);
 
