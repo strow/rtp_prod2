1aa552e50e84441168a5aae987889180d1756306
Steven Buczkowski <steven.buczkowski@gmail.com>
2015-03-25 15:10:42 -0400

    Second commit for branch cleanup. This reorganizes at the rtp_prod2/airs level.

---
+++ airs/genscratchpath.m
@@ -0,0 +1,20 @@
+function [sNodeID, sTempPath] = genscratchpath()
+% GENSCRATCHPATH  Generate path to scratch/temp file space
+%
+% Takes into account differences in behavior between slurm and
+% non-slurm job execution and the differing placement of
+% temporary/scratch space between cluster compute nodes and cluster
+% head node/non-cluster machines
+%
+
+sNodeID = getenv('SLURM_PROCID');
+sScratchPath = getenv('JOB_SCRATCH_DIR');
+if ~isempty(sNodeID) && ~isempty(sScratchPath)
+    sTempPath = sScratchPath;
+else
+    sTempPath = '/tmp';
+    rng('shuffle');
+    sNodeID = sprintf('%03d', randi(999));
+end
+
+end
