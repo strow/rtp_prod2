c69847db704d4b01e379f5d415b8ae847e433063
Steven Buczkowski <steven.buczkowski@gmail.com>
2015-06-05 14:12:47 -0400

    Fixed a few issues to make CrIS processing world accessible

--- cris/scripts/run_cris_batch.m
+++ cris/scripts/run_cris_batch.m
@@ -1,6 +1,6 @@
 function run_cris_batch()
-
-addpath(genpath('/home/sbuczko1/git/rtp_prod2'));
+set_process_dirs;
+addpath(genpath(rtp_sw_dir));
 
 % grab the slurm array index for this process
 slurmindex = str2num(getenv('SLURM_ARRAY_TASK_ID'));
@@ -12,14 +12,14 @@ slurmindex = str2num(getenv('SLURM_ARRAY_TASK_ID'));
 %    ls -d1 $PWD/{048,049,050}/*.mat >> ~/cris-files-process.txt
 %
 % cris-files-process.txt, then, contains lines like:
-%    /asl/data/cris/ccast/sdr60_hr/2015/048/SDR_d20150217_t1126169.matq
-[status, infile] = system(sprintf('sed -n "%dp" ~/cris-files-process.txt | tr -d "\n"', ...
-                                  slurmindex));
+%    /asl/data/cris/ccast/sdr60_hr/2015/048/SDR_d20150217_t1126169.mat
+[status, infile] = system(sprintf('sed -n "%dp" %s | tr -d "\n"', ...
+                                  slurmindex, cris_ccast_file_list));
 
 % separate out parts of file path. We want to keep the bulk of the
 % filename intact but change SDR -> rtp and change the extension to
 % rtp as well as we make the output file path
-outpath='/asl/data/rtp_cris_slb';
+outpath=cris_ccast_rtp_out_dir;
 [path, name, ext] = fileparts(infile);
 outfile = fullfile(outpath, [strrep(name, 'SDR', 'rtp') '.rtp']);
 
--- cris/scripts/set_process_dirs.m
+++ cris/scripts/set_process_dirs.m
@@ -1,3 +1,3 @@
 rtp_sw_dir   = '~/git/rtp_prod2';
 cris_ccast_rtp_out_dir = '/asl/data/cris_ccast_rtp';
-
+cris_ccast_file_list = '~/cris-files-to-process';
