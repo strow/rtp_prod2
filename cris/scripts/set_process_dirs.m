f23a86b9f8a5ab9dc415d47e00304332bded4825
Steven Buczkowski <steven.buczkowski@gmail.com>
2015-09-25 16:52:27 -0400

    Mod to ensure rtp runs with ECMWF
    hires allfov and clear/dcc/site/random with ecmwf model

--- cris/scripts/set_process_dirs.m
+++ cris/scripts/set_process_dirs.m
@@ -1,3 +1,1 @@
-rtp_sw_dir   = '~/git/rtp_prod2';
-cris_ccast_rtp_out_dir = '/asl/s1/sbuczko1/testoutput';
-cris_ccast_file_list = '~/cris-files-to-process';
+rtp_sw_dir   = '/asl/packages/rtp_prod2/cris/scripts';
