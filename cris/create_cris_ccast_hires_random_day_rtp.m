0720683b236d87ae128bb95117fae252912ac3e5
Steven Buczkowski <steven.buczkowski@gmail.com>
2018-07-30 08:29:32 -0400

    Cleaning up random day code

--- cris/create_cris_ccast_hires_random_day_rtp.m
+++ cris/create_cris_ccast_hires_random_day_rtp.m
@@ -6,13 +6,15 @@ funcname = 'create_cris_ccast_hires_random_day_rtp';
 
 fprintf(1, '>> Running %s for input: %s\n', funcname, fnCrisInput);
 
-addpath(genpath('/asl/matlib'));
+addpath ./util
+addpath ../emis;  % add_emis
+addpath ../util;  % rtpread/write
+addpath ../grib;  % fill_era/ecmwf
+% $$$ addpath(genpath('/asl/matlib'));
 addpath /asl/matlib/aslutil   % int2bits
 addpath /asl/packages/time    % iet2tai (in ccast2rtp)
 addpath /home/sbuczko1/git/ccast/motmsc/rtp_sarta  % ccast2rtp
-addpath /home/sbuczko1/git/rtp_prod2/grib;  % fill_era/ecmwf
-addpath /asl/packages/rtp_prod2/emis;  % add_emis
-addpath /asl/packages/rtp_prod2/util;  % rtpread/write
+addpath /home/sbuczko1/git/ccast/source  % fixmyQC
 addpath /home/sbuczko1/git/matlib/clouds/sarta  %
                                                 % driver_cloud... (version
                                                 % in /asl/matlib
@@ -25,8 +27,6 @@ cfg.sTempPath = sTempPath;
 
 % read in configuration options from 'cfg'
 klayers_exec = '/asl/packages/klayersV205/BinV201/klayers_airs_wetwater';
-% $$$ sarta_exec  = ['/asl/packages/sartaV108/BinV201/' ...
-% $$$                'sarta_iasi_may09_wcon_nte'];
 sarta_exec = '/asl/bin/crisg4_oct16';
 sartacld_exec = ['/asl/bin/' ...
                  'crisg4_hires_dec17_iceGHMbaum_waterdrop_desertdust_slabcloud_hg3'];
@@ -77,81 +77,79 @@ if numgrans ~= 0
     fprintf(1,'>>> %s Found %d granule files to process\n', ...
             char(datetime('now', 'Format', 'HHmmss')), numel(fnLst1));
 else
-    fprintf(2, ['>>> %s ERROR: No granules files found for day %s. ' ...
+    fprintf(2, ['>>>> %s ERROR: No granules files found for day %s. ' ...
                 'Exiting.\n'], char(datetime('now', 'Format', 'HHmmss')), fnCrisInput);
     return;
 end
 
-    
 bFirstGranRead = false;
-for i=1:numel(fnLst1)
-    % try reading the granule files and concatenate profiles for
+stride=1;
+for i=1:stride:numel(fnLst1)
+    % try reading the granule files and concatenate piles for
     % each granule read
+    fprintf(1, '>>> Reading granule #%d/%d\n', i, numel(fnLst1));
     try
-        [h2, ha2, p2, pa2] = ccast2rtp(fullfile(fnCrisInput,fnLst1(i).name), nguard);
+        [h,ha,p,pa] = ccast2rtp(fullfile(fnCrisInput,fnLst1(i).name), nguard);
     catch
-        fprintf(2, ['>>> %s ERROR: ccast2rtp failure. Trying ' ...
+        fprintf(2, ['>>>> %s ERROR: ccast2rtp failure. Trying ' ...
                     'next granule\n'], char(datetime('now', 'Format', 'HHmmss')));
         continue;
     end
-    if length(prof.rtime) == 0
+    if length(p.rtime) == 0
         return
     end
 
     % check ichan index order (to avoid problems with rtpwrite)
-    temp = size(head.ichan);
+    temp = size(h.ichan);
     if temp(2) > 1
-        head.ichan = head.ichan';
+        h.ichan = h.ichan';
     end
-    temp = size(head.vchan);
+    temp = size(h.vchan);
     if temp(2) > 1
-    head.vchan = head.vchan';
+    h.vchan = h.vchan';
     end
 
     % filter out desired FOVs/scan angles
-    fprintf(1, '>>> Running get_equal_area_sub_indices for random selection... ');
-% $$$ fors = [15 16];  % Indices for desired Fields of Regard (FOR)
+    fprintf(1, '>>> Running get_equal_area_sub_indices for random selection... \n');
     fors = [1:30];
 
-    nadir = ismember(prof.xtrack,fors);
+    nadir = ismember(p.xtrack,fors);
 
     % rtp has a 2GB limit so we have to scale number of kept FOVs
     % to stay within that as an absolute limit. Further, we
     % currently restrict obs count in random to ~20k to match
     % historical AIRXBCAL processing
-    limit = 95000;  % number of obs to keep
+    limit = 65000;  % number of obs to keep
     nswath = 45;  % length of ccast granules
     ngrans = 240;  % number of granules per day
     nfovs = 9;  % number of FOVs per FOR
     maxobs = nswath * length(fors) * nfovs * ngrans;
-    scale = (limit/maxobs)*1.6; % preserves ~95k obs/day 
-    randoms = get_equal_area_sub_indices(prof.rlat, scale);
+    scale = (limit/maxobs)*1.6; % preserves ~65k obs/day 
+    randoms = get_equal_area_sub_indices(p.rlat, scale);
     nrinds = find(nadir & randoms);
     if length(nrinds) == 0
         return
     end
-    crprof = rtp_sub_prof(prof, nrinds);
-    prof=crprof;
-    clear crprof;
+    p = rtp_sub_prof(p, nrinds);
 
     % build sub satellite lat point
-    [prof, pattr] = build_satlat(prof,pattr);
+    [p, pa] = build_satlat(p,pa);
 
     % Need this later
-    ichan_ccast = head.ichan;
+    ichan_ccast = h.ichan;
 
     % now that we've trimmed off many (most?) of the obs,
     % concatenate into a single daily rtp structure
     if ~bFirstGranRead % this is first good read. Take rtp structs
                        % as baseline for subsequent concatenation
-        h = h2;
-        ha = ha2;
-        p = p2;
-        pa = pa2;
+        head = h;
+        hattr = ha;
+        prof = p;
+        pattr = pa;
         
         bFirstGranRead = true;
     else
-        [h, p] = cat_rtp(h, p, h2, p2);
+        [head, prof] = cat_rtp(head, prof, h, p);
     end
 
 end % end loop over granules
@@ -186,62 +184,16 @@ fprintf(1, '>>> Running rtp_add_emis_single...');
 [prof,pattr] = rtp_add_emis_single(prof,pattr);
 fprintf(1, 'Done\n');
 
-% run klayers
-fn_rtp1 = fullfile(sTempPath, ['cris_' sID '_1.rtp']);
-fprintf(1, '>>> Writing klayers input temp file %s ...', fn_rtp1);
-rtpwrite(fn_rtp1,head,hattr,prof,pattr)
-fprintf(1, 'Done\n')
-fn_rtp2 = fullfile(sTempPath, ['cris_' sID '_2.rtp']);
-run_klayers=[klayers_exec ' fin=' fn_rtp1 ' fout=' fn_rtp2 ' > ' sTempPath ...
-             '/klayers_' sID '_stdout']
-fprintf(1, '>>> Running klayers: %s ...', run_klayers);
-unix([klayers_exec ' fin=' fn_rtp1 ' fout=' fn_rtp2 ' > ' sTempPath ...
-      '/klayers_' sID '_stdout'])
-fprintf(1, 'Done\n');
-
-% Run sarta
-if strcmp('csarta', cfg.rta)
-    % *** split fn_rtp3 into 'N' multiple chunks (via rtp_sub_prof like
-    % below for clear,site,etc?) make call to external shell script to
-    % run 'N' copies of sarta backgrounded
-    fn_rtp3 = fullfile(sTempPath, ['cris_' sID '_3.rtp']);
-    run_sarta = [sarta_exec ' fin=' fn_rtp2 ' fout=' fn_rtp3 ' > ' ...
-                 sTempPath '/sarta_' sID '_stdout.txt'];
-    fprintf(1, '>>> Running sarta: %s ...', run_sarta);
-    unix(run_sarta);
-    fprintf(1, 'Done\n');
-    
-    % Read in new rcalcs and insert into origin prof field
-% $$$ stFileInfo = dir(fn_rtp3);
-% $$$ fprintf(1, ['*************\n>>> Reading fn_rtp3:\n\tName:\t%s\n\tSize ' ...
-% $$$             '(GB):\t%f\n*************\n'], stFileInfo.name, stFileInfo.bytes/1.0e9);
-    fprintf(1, '>>> Reading sarta output... ');
-    [h,ha,p,pa] = rtpread(fn_rtp3);
-    fprintf(1, 'Done\n');
-    
-    % Go get output from klayers, which is what we want except for rcalc
-% $$$ [head, hattr, prof, pattr] = rtpread(fn_rtp2);
-% Insert rcalc for CrIS derived from IASI SARTA
-    prof.rclr = p.rcalc;
-    head.pfields = 7;
-    
-% $$$ 
 % $$$ % run driver_sarta_cloud to handle klayers and sarta runs
-    sarta_cfg.clear=+1;
-    sarta_cfg.cloud=+1;
-    sarta_cfg.cumsum=-1;
-    sarta_cfg.klayers_code = klayers_exec;
-    sarta_cfg.sartaclear_code = sarta_exec;
-    sarta_cfg.sartacloud_code = sartacld_exec;
-    
-    prof = driver_sarta_cloud_rtp(head, hattr, prof, pattr, ...
-                                  sarta_cfg);
-else if strcmp('isarta', cfg.rta)
-        [head, hattr, prof, pattr] = rtpread(fn_rtp2);
-        [head, hattr, prof, pattr] = run_sarta_iasi(head, hattr, ...
-                                                    prof, pattr, ...
-                                                    cfg);
-end
+sarta_cfg.clear=+1;
+sarta_cfg.cloud=+1;
+sarta_cfg.cumsum=+9999;
+sarta_cfg.klayers_code = klayers_exec;
+sarta_cfg.sartaclear_code = sarta_exec;
+sarta_cfg.sartacloud_code = sartacld_exec;
+
+prof = driver_sarta_cloud_rtp(head, hattr, prof, pattr, ...
+                              sarta_cfg);
 
 end  % end function
 
