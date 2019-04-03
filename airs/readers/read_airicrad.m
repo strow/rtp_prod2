ed1178b7114b2a9bbad8fc1ac8eb70a467a54e8b
Steven Buczkowski <sbuczko1@umbc.edu>
2019-04-03 13:04:24 -0400

    AIRS: undoing change of scan_node_type A/D -> 0/1. Keep as characters

--- airs/readers/read_airicrad.m
+++ airs/readers/read_airicrad.m
@@ -192,7 +192,6 @@ gdata.iudef(3,:) = junk2(i0);
 %
 junk = cell2mat(hdfread(fn, 'scan_node_type'));
 junk2 = reshape( (ones(90,1)*double(junk))', 1,nobs);
-junk2 = (junk2 == 68); % change A/D to 0/1
 gdata.iudef(4,:) = junk2(i0);
 %
 % $$$ junk = permute(hdfread(fn, 'AB_Weight'), [3 2 1]);
