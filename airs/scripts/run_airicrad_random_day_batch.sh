62a52eac20e49ecee05476fc2ad0c1394dcca26b
Steven Buczkowski <sbuczko1@umbc.edu>
2020-06-18 14:19:51 -0400

    AIRS: forgot to incorporate changes to random driver script

--- airs/scripts/run_airicrad_random_day_batch.sh
+++ airs/scripts/run_airicrad_random_day_batch.sh
@@ -7,18 +7,18 @@
 # sbatch options
 #SBATCH --job-name=RUN_CREATE_AIRS_RANDOM_DAY_RTP
 # partition = dev/batch
-#SBATCH --partition=batch
+#SBATCH --partition=high_mem
 # qos = short/normal/medium/long/long_contrib
-#SBATCH --qos=normal+
+#SBATCH --qos=medium+
 #SBATCH --account=pi_strow
 #SBATCH -N1
 #SBATCH --mem-per-cpu=18000
 #SBATCH --cpus-per-task 1
 ##SBATCH --array=0-179
-#SBATCH --time=01:59:00
+#SBATCH --time=07:59:00
 
-#SBATCH -o /home/sbuczko1/logs/sbatch/run_create_airs_random_day-%A_%a.out
-#SBATCH -e /home/sbuczko1/logs/sbatch/run_create_airs_random_day-%A_%a.err
+#SBATCH -o /home/sbuczko1/LOGS/sbatch/run_create_airs_random_day-%A_%a.out
+#SBATCH -e /home/sbuczko1/LOGS/sbatch/run_create_airs_random_day-%A_%a.err
 
 # matlab options
 MATLAB=matlab
